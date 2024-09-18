1ca09d8894d1b3e0d390def2eac5a42bcb82e61e
Sato-007 <satyaki.bose3@gmail.com>
2024-09-17 23:17:15 -0400

    Working source 01

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -7,16 +7,54 @@ let users = [];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+    let userswithsamename = users.filter((user) => {
+        return user.username === username;
+    });
+
+    if (userswithsamename.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+    let validusers = users.filter((user) => {
+        return (user.username === username && user.password === password)
+    });
+
+    if (validusers.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.params.username;
+  const password = req.params.password;
+
+  if (!username || !password) {
+    res.send.status(404).json({ message : "Error loging in" })
+  }
+
+  if (authenticatedUser(username,password)) {
+    let accessToken = jwt.sign({
+        data: password
+    },'access', { expiresIn: 60 * 60 });
+
+    req.session.authorization = {
+        accessToken, username
+    }
+
+    return res.status(200).json({ message: "User successfully logged in" });
+  }else {
+    return res.status(208).json({ message: "Invalid Login. Check username and password" });
+  }
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Add a book review
